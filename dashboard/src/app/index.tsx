f6228867942dfbcfa7d796ab240f00eeb9f001df
Ilya Azin <martis.azin@gmail.com>
2021-05-11 08:27:01 +0300

    feat(mvp): proto app

---
+++ dashboard/src/app/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss"
+
+const App = () => {
+    return (
+        <div className={styles.root}>
+            Hello, Projentry!
+        </div>
+    )
+}
+
+export default App
--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app';
 import reportWebVitals from './reportWebVitals';
+import "normalize.css";
 
 ReactDOM.render(
   <React.StrictMode>
