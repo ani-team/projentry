dab1ea42537a3c9ed6a94daca008d00bf3d3423f
Ilya Azin <martis.azin@gmail.com>
2021-05-11 09:43:24 +0300

    feat(mvp): proto board

--- dashboard/src/pages/board/index.tsx
+++ dashboard/src/pages/board/index.tsx
@@ -1,14 +1,48 @@
-import { Typography, Layout } from "antd";
+import { Typography, Layout, Card, Row, Col } from "antd";
+import {
+    RocketOutlined,
+    BugOutlined,
+    ApiOutlined,
+    QuestionCircleOutlined,
+} from "@ant-design/icons";
+import type { ReactNode } from "react";
 
 import { Header, Footer } from "features";
 import { dom } from "shared/lib";
 import styles from "./styles.module.scss";
 
-// !!! FIXME: split by features!
-// TODO: Add skeletons loader
+type Topic = {
+    title: string;
+    description: string;
+    icon: ReactNode;
+};
+
+const topics: Topic[] = [
+    {
+        title: "Get Started",
+        description: "Onboaridng, setup env, ...",
+        icon: <RocketOutlined style={{ fontSize: 20 }} />,
+    },
+    {
+        title: "Project Health",
+        description: "Issues, backlog, tech debt ...",
+        // icon: <WarningOutlined style={{ fontSize: 20 }} />,
+        icon: <BugOutlined style={{ fontSize: 20 }} />,
+    },
+    {
+        title: "FAQ",
+        description: "Frequently Asked Questions",
+        icon: <QuestionCircleOutlined style={{ fontSize: 20 }} />,
+    },
+    {
+        title: "Conventions",
+        description: "Code, structure, architecture",
+        icon: <ApiOutlined style={{ fontSize: 20 }} />,
+    },
+];
 
 /**
- * @page Дешборд страница
+ * @page Дашборд страница
  */
 const BoardPage = () => {
     dom.useTitle("Dashboard — MyProj");
@@ -20,7 +54,20 @@ const BoardPage = () => {
                 <Typography.Title className={styles.title} level={2}>
                     MyProj
                 </Typography.Title>
-                <Layout className={styles.catalog}>...</Layout>
+                <Typography.Title className={styles.title} level={4} type="secondary">
+                    Project dashboard
+                </Typography.Title>
+                <Layout className={styles.content}>
+                    <Row className={styles.topics} gutter={[20, 20]}>
+                        {topics.map((t) => (
+                            <Col key={t.title} span={12}>
+                                <Card title={t.title} extra={t.icon} hoverable>
+                                    <p>{t.description}</p>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Layout>
             </Layout.Content>
             <Footer />
         </Layout>
