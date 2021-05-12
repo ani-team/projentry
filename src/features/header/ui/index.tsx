48eddbbe4286ab75f023c22a6a43f3d34ead55aa
Ilya Azin <martis.azin@gmail.com>
2021-05-12 11:19:07 +0300

    feat(mvp): refine header (toolbar)

--- src/features/header/ui/index.tsx
+++ src/features/header/ui/index.tsx
@@ -1,7 +1,10 @@
-import { Layout } from "antd";
-import { QqOutlined } from "@ant-design/icons";
+import { Layout, Switch } from "antd";
+import Icon, { QqOutlined, GithubFilled } from "@ant-design/icons";
 import cn from "classnames";
 import { Link } from "react-router-dom";
+
+import { ReactComponent as IconMoon } from "./moon.svg";
+import { ReactComponent as IconSun } from "./sun.svg";
 import styles from "./styles.module.scss";
 
 type Props = {
@@ -20,7 +23,22 @@ const Header = (props: Props) => {
                 <h1 className={styles.logoTitle}>projentry</h1>
             </Link>
             <div className={styles.search}>SEARCH</div>
-            <div className={styles.toolbar}>TOOLBAR</div>
+            <div className={styles.toolbar}>
+                <Switch
+                    className={styles.switch}
+                    checkedChildren={<Icon component={IconMoon} className={styles.switchIcon} />}
+                    unCheckedChildren={<Icon component={IconSun} className={styles.switchIcon} />}
+                    disabled
+                />
+                <a
+                    className={styles.github}
+                    href="https://github.com/ani-team"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <GithubFilled style={{ fontSize: 30 }} />
+                </a>
+            </div>
         </Layout.Header>
     );
 };
