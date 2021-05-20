a689b8d06aea2fa00dc5f983ef2fc70b8827e8ad
Ilya Azin <martis.azin@gmail.com>
2021-05-21 00:06:23 +0300

    feat: add subpages navigation at HealthIndex

--- dashboard/src/pages/health/index/index.tsx
+++ dashboard/src/pages/health/index/index.tsx
@@ -1,22 +1,16 @@
-import { Typography, Layout, Breadcrumb } from "antd";
-import { useLocation, Link } from "react-router-dom";
+import { Typography, Layout, Card, Col, Row, Empty } from "antd";
+import cn from "classnames";
+import { Link } from "react-router-dom";
 
 import { Header } from "features";
-import * as topics from "entities/topic";
-import { dom, string } from "shared/lib";
+import { dom } from "shared/lib";
 import { Split } from "shared/ui";
+import { routes } from "../routes";
+import styles from "./styles.module.scss";
 
-/**
- * @page Страница заглушка для топика
- */
 // eslint-disable-next-line max-lines-per-function
-const TopicPage = () => {
-    const location = useLocation();
-    const slug = location.pathname.slice(1);
-    const topicName = string.unslugize(slug);
-    dom.useTitle(`${topicName} | MyProj`);
-
-    const topic = topics.findByTitleSlug(slug);
+const HealthIndexPage = () => {
+    dom.useTitle(`Health | MyProj`);
 
     return (
         <Split header={<Header />}>
@@ -24,11 +18,31 @@ const TopicPage = () => {
                 <Typography.Title className="mt-40" level={2}>
                     Project Health
                 </Typography.Title>
-                <Layout className="mt-40">{topic?.description}</Layout>
+                <Layout className="mt-40">
+                    <Row gutter={[0, 20]}>
+                        {routes.map((route) => (
+                            <Col
+                                key={route.title}
+                                span={24}
+                                className={cn(route.disabled && styles.itemDisabled)}
+                            >
+                                <Link to={route.href}>
+                                    <Card title={route.title} hoverable>
+                                        <p>{route.description}</p>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        ))}
+                    </Row>
+                </Layout>
             </Split.Main>
-            <Split.Sider>Выберите страницу для продолжения</Split.Sider>
+            <Split.Sider>
+                <Row className="h100" align="middle" justify="center">
+                    <Empty description="Select section for continue" />
+                </Row>
+            </Split.Sider>
         </Split>
     );
 };
 
-export default TopicPage;
+export default HealthIndexPage;
---
+++ dashboard/src/pages/health/routes.ts
@@ -0,0 +1,14 @@
+export const routes = [
+    {
+        title: "Issues",
+        description: "Tech debt project issues",
+        href: "/health/issues",
+        disabled: false,
+    },
+    {
+        title: "Backlog",
+        description: "Project backlog",
+        href: "/health/backlog",
+        disabled: true,
+    },
+];
