7d740798d584af7d230f2dcb3009d240fa55d807
Ilya Azin <martis.azin@gmail.com>
2021-05-22 19:17:44 +0300

    feat(docs): add article details

--- dashboard/src/pages/docs/section/index.tsx
+++ dashboard/src/pages/docs/section/index.tsx
@@ -1,14 +1,13 @@
-import { Typography, Layout, Row, Col, Divider } from "antd";
+import { Typography, Layout, Row, Col, Card } from "antd";
 import { FileFilled } from "@ant-design/icons";
-import { useLocation } from "react-router-dom";
 
 import { Header } from "features";
 import * as topic from "entities/topic";
 import { NavBreadcrumb } from "entities/navigation";
+import { getFile } from "shared/api";
 import { dom } from "shared/lib";
 import { docs } from "shared/config";
-import type { DocSection } from "shared/config";
-import { Split, RowCard } from "shared/ui";
+import { Split, RowCard, Markdown } from "shared/ui";
 
 type Props = import("react-router-dom").RouteChildrenProps<{
     section: string;
@@ -22,69 +21,74 @@ const topicsMap: Record<string, topic.Topic> = {
     "faq": topic.FAQ,
 };
 // FIXME: @temp
-const articlesMap: Record<string, DocSection> = {
+const articlesMap: Record<string, import("shared/config").DocSection> = {
     "get-started": docs.getStarted,
     "conventions": docs.conventions,
     "faq": docs.faq,
 };
 
-const useSection = (sectionSlug: string) => {
+// FIXME: @complexity @decompose
+const useRouteMapping = (props: Props) => {
+    const routeParams = props.match!.params;
+    const sectionSlug = routeParams.section;
+    const articleSlug = routeParams.article ? decodeURIComponent(routeParams.article) : undefined;
+
     // FIXME: @temp move to entities/navigation
     const section = topicsMap[sectionSlug];
     const articles = articlesMap[sectionSlug];
-    return { section, articles };
+    return {
+        data: { section, articles },
+        route: { sectionSlug, articleSlug },
+    };
 };
 
 // FIXME: @hardcoded @temp
 // eslint-disable-next-line max-lines-per-function
 const SectionPage = (props: Props) => {
-    const routeParams = props.match!.params;
-    const { section, articles } = useSection(routeParams?.section);
+    const { data, route } = useRouteMapping(props);
+    dom.useProjectTitle(data.section.title);
 
-    dom.useProjectTitle(section.title);
+    const article = route.articleSlug ? getFile(route.articleSlug) : undefined;
 
     return (
         <Split header={<Header />}>
             <Split.Main>
                 <NavBreadcrumb />
                 <Typography.Title className="mt-40" level={2}>
-                    {section.title}
+                    {data.section.title}
                 </Typography.Title>
-                <Typography.Text type="secondary">{section.description}</Typography.Text>
+                <Typography.Text type="secondary">{data.section.description}</Typography.Text>
                 <Layout className="mt-40">
                     <Row gutter={[0, 20]}>
-                        {articles.paths.map((path) => (
+                        {data.articles.paths.map((path) => (
                             <Col key={path} span={24}>
                                 {/* TODO: add active logic */}
                                 {/* TODO: add article detect logic */}
-                                <RowCard Icon={FileFilled as any} subtitle={path} title={path} />
+                                <RowCard
+                                    Icon={FileFilled as any}
+                                    subtitle={path}
+                                    title={path}
+                                    href={`/docs/${route.sectionSlug}/${encodeURIComponent(path)}`}
+                                    active={route.articleSlug === path}
+                                />
                             </Col>
                         ))}
                     </Row>
                 </Layout>
             </Split.Main>
             <Split.Sider>
-                {/* {issue && (
-                    <article>
-                        <IssueCard data={issue} />
-                        <IssueStat data={issue} />
-                        <Divider />
-                        <Row gutter={[0, 20]}>
-                            {Object.values(PATHS.CODE).map((pathname) => (
-                                <Col key={pathname} span={24}>
-                                    <File.Preview
-                                        pathname={pathname}
-                                        content={
-                                            getFileIssueSnippets(pathname, issue?.tag || "")
-                                                .issueSnippets
-                                        }
-                                    />
-                                </Col>
-                            ))}
-                        </Row>
-                    </article>
+                {/* FIXME: @decompose move to /entities/article */}
+                {article && (
+                    <Card
+                        type="inner"
+                        title={route.articleSlug}
+                        className="mt-20"
+                        bodyStyle={{ minHeight: 500, overflow: "hidden" }}
+                    >
+                        <Markdown text={article} />
+                    </Card>
                 )}
-                {!issue && <Split.Placeholder title="Select doc for continue" />} */}
+                {!article && <Split.Placeholder title="Select article for continue" />}
             </Split.Sider>
         </Split>
     );
