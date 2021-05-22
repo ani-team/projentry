7e9b3e1a28861f7720d485c54d2b66fe8a4f83ee
Ilya Azin <martis.azin@gmail.com>
2021-05-22 18:04:19 +0300

    refactor(topics): decompose each topic

--- dashboard/src/entities/topic/lib.tsx
+++ dashboard/src/entities/topic/lib.tsx
@@ -13,40 +13,59 @@ export type Topic = {
     title: string;
     description: string;
     icon: ReactNode;
+    href: string;
 };
 
+export const GET_STARTED: Topic = {
+    title: "Get Started",
+    description: "Onboarding, setup env, quick-start",
+    icon: <RocketOutlined style={{ fontSize: 20 }} />,
+    href: "/docs/get-started",
+};
+export const CONVENTIONS: Topic = {
+    title: "Conventions",
+    description: "Code, structure, architecture",
+    icon: <ApiOutlined style={{ fontSize: 20 }} />,
+    href: "/docs/conventions",
+};
+
+export const FAQ: Topic = {
+    title: "FAQ",
+    description: "Frequently Asked Questions",
+    icon: <QuestionCircleOutlined style={{ fontSize: 20 }} />,
+    href: "/docs/faq",
+};
+
+export const HEALTH: Topic = {
+    title: "Health",
+    description: "Issues tech debt, backlog",
+    icon: <WarningOutlined style={{ fontSize: 20 }} />,
+    // icon: <BugOutlined style={{ fontSize: 20 }} />,
+    href: "/health",
+};
+
+export const TECH: Topic = {
+    title: "Tech",
+    description: "Tech stack, dependencies",
+    icon: <ToolOutlined style={{ fontSize: 20 }} />,
+    href: "/tech",
+};
+
+export const EXPLORER: Topic = {
+    title: "Explorer",
+    description: "Project map, inner READMEs",
+    icon: <FileSearchOutlined style={{ fontSize: 20 }} />,
+    href: "/explorer",
+};
+
+// prettier-ignore
 export const topics: Topic[] = [
-    {
-        title: "Get Started",
-        description: "Onboarding, setup env, quick-start",
-        icon: <RocketOutlined style={{ fontSize: 20 }} />,
-    },
-    {
-        title: "Conventions",
-        description: "Code, structure, architecture",
-        icon: <ApiOutlined style={{ fontSize: 20 }} />,
-    },
-    {
-        title: "FAQ",
-        description: "Frequently Asked Questions",
-        icon: <QuestionCircleOutlined style={{ fontSize: 20 }} />,
-    },
-    {
-        title: "Health",
-        description: "Issues tech debt, backlog",
-        icon: <WarningOutlined style={{ fontSize: 20 }} />,
-        // icon: <BugOutlined style={{ fontSize: 20 }} />,
-    },
-    {
-        title: "Tech",
-        description: "Tech stack, dependencies",
-        icon: <ToolOutlined style={{ fontSize: 20 }} />,
-    },
-    {
-        title: "Explorer",
-        description: "Project map, inner READMEs",
-        icon: <FileSearchOutlined style={{ fontSize: 20 }} />,
-    },
+    GET_STARTED,
+    CONVENTIONS,
+    FAQ,
+    HEALTH,
+    TECH,
+    EXPLORER,
 ];
 
 export const findByTitleSlug = (term: string) => {
