ad0bc45d91a432cae6a49d7154ca3742d3cd80ab
Ilya Azin <martis.azin@gmail.com>
2021-05-22 18:32:16 +0300

    feat(docs): add section detection

--- dashboard/src/pages/docs/section/index.tsx
+++ dashboard/src/pages/docs/section/index.tsx
@@ -1,29 +1,44 @@
 import { Typography, Layout, Row, Col, Divider } from "antd";
-import { RouteChildrenProps } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import { Header } from "features";
 import { File } from "entities/file";
-import { IssueRow, IssueCard, IssueStat } from "entities/issue";
+import * as topic from "entities/topic";
 import { NavBreadcrumb } from "entities/navigation";
 import { dom } from "shared/lib";
 import { getFileIssueSnippets } from "shared/api";
 import { issuesTypes, findIssue, PATHS } from "shared/config";
 import { Split } from "shared/ui";
 
+// FIXME: @temp
+const topicsMap: Record<string, topic.Topic> = {
+    "get-started": topic.GET_STARTED,
+    "conventions": topic.CONVENTIONS,
+    "faq": topic.FAQ,
+};
+
+const useSection = () => {
+    // FIXME: @temp move to entities/navigation
+    const location = useLocation();
+    const slug = location.pathname.split("/").pop()!;
+    return topicsMap[slug];
+};
+
+// FIXME: @hardcoded @temp
 // eslint-disable-next-line max-lines-per-function
 const SectionPage = () => {
-    dom.useProjectTitle("Section");
-
-    // const issue = params?.issueTag ? findIssue(params.issueTag) : undefined;
+    const section = useSection();
+    dom.useProjectTitle(section.title);
 
     return (
         <Split header={<Header />}>
             <Split.Main>
                 <NavBreadcrumb />
                 <Typography.Title className="mt-40" level={2}>
-                    Section
+                    {section.title}
                 </Typography.Title>
                 <Layout className="mt-40">
+                    {section.description}
                     {/* <Row gutter={[0, 20]}>
                         {issuesTypes.map((issueType) => (
                             <Col key={issueType.tag} span={24}>
