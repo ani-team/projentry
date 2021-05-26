f544be02c42e1bc6582b46be7cecdf818bd64404
Ilya Azin <martis.azin@gmail.com>
2021-05-27 01:35:14 +0300

    chore: update stand (19)

--- src/shared/config/paths.ts
+++ src/shared/config/paths.ts
@@ -1,17 +1,17 @@
 export const PATHS = {
     CODE: {
-        AUTH_HOOK: "../test/src/features/auth/hooks/index.tsx",
-        STYLES_1: "../test/src/features/origin/styles.scss",
-        TASK_DETAILS: "../test/src/pages/task-details/index.jsx",
+        AUTH_HOOK: "/src/features/auth/hooks/index.tsx",
+        STYLES_1: "/src/features/origin/styles.scss",
+        TASK_DETAILS: "/src/pages/task-details/index.jsx",
     },
     DOCS: {
         // GET_STARTED: "/docs/get-started.md",
-        SETUP_ENV: "../test/docs/setup-env.md",
-        DEV_GUIDE: "../test/docs/dev-guide.md",
-        PAGES: "../test/src/pages/readme.md",
-        FEATURES: "../test/src/features/readme.md",
-        CODE_STYLE: "../test/docs/code-style.md",
-        STANDS: "../test/STANDS.md",
-        CODEGEN: "../test/.graphql/readme.md",
+        SETUP_ENV: "/docs/setup-env.md",
+        DEV_GUIDE: "/docs/dev-guide.md",
+        PAGES: "/src/pages/readme.md",
+        FEATURES: "/src/features/readme.md",
+        CODE_STYLE: "/docs/code-style.md",
+        STANDS: "/STANDS.md",
+        CODEGEN: "/.graphql/readme.md",
     },
 };
